b7bf03f21e870e041674ec9e764f50ae6166be3c
bolotovruslan <lancer11.com@gmail.com>
2021-12-03 15:45:23 +0300

    add refactor budy

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ApplicationRecord
+  before_save :refactor_body
+  before_update :refactor_body
+
   TITLEPAGE_FORMAT = /\A[а-яА-ЯёЁa-zA-Z0-9_]+\z/
 
   validates :title, presence: true, format: { with: TITLEPAGE_FORMAT }
@@ -7,4 +10,11 @@ class Page < ApplicationRecord
 
   has_ancestry
 
+  def refactor_body
+    self.body = body.gsub(/\*\*(?<text>.*)\*\*/, '<b>\k<text></b>')
+    self.body = body.gsub(/\\\\(?<text>.*)\\\\/, '<i>\k<text></i>')
+    self.body = body.gsub(/\(\((?<site>.*) (?<text>.*)\)\)/, '<a href="/\k<site>">\k<text></a>')
+
+    self.body.html_safe
+  end
 end
